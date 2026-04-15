3a50c25a4c86746ce24b4d57246855197e56be92
Loic GIRAUD <lgiraud.giraud@gmail.com>
2026-04-15 10:15:36 +0200

    back to normal

--- numja/src/test/java/fr/uga/numja/NdarrayTest.java
+++ numja/src/test/java/fr/uga/numja/NdarrayTest.java
@@ -12,5 +12,48 @@ public class NdarrayTest {
         Ndarray.zeros(0);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateZerosNdarray2DInvalidCols(){
+        Ndarray.zeros(1, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateZerosNdarray2DInvalidRows(){
+        Ndarray.zeros(-1, 10);
+    }
+
+    @Test
+    public void testCreateZerosNdarry1D(){
+        Ndarray array = Ndarray.zeros(10);
+        int[] shape = new int[2];
+        shape[0] = 10;
+        shape[1] = 0;
+        assertEquals("Created array should have size", 10, array.getSize());
+        assertEquals("Created array should have dimension ", 1, array.getNdim());
+        assertArrayEquals("Created array should have the shape", shape, array.getShape());
+    }
+
+    @Test
+    public void testCreateZerosNdarry2D(){
+        Ndarray array = Ndarray.zeros(10, 10);
+        int[] shape = new int[2];
+        shape[0] = 10;
+        shape[1] = 10;
+        assertEquals("Created array should have size", 100, array.getSize());
+        assertEquals("Created array should have dimension ", 2, array.getNdim());
+        assertArrayEquals("Created array should have the shape", shape, array.getShape());
+    }
+
+    @Test
+    public void testToStringZerosNdarray1D(){
+        Ndarray array = Ndarray.zeros(10);
+        assertEquals("Array should have 10 zeros", "[0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ]", array.toString());
+    }
+
+    @Test
+    public void testToStringZerosNdarray2D(){
+        Ndarray array = Ndarray.zeros(2,5);
+        assertEquals("Array should have 2 times 5 zeros", "[[0.0 , 0.0 , 0.0 , 0.0 , 0.0 ],\n[0.0 , 0.0 , 0.0 , 0.0 , 0.0 ]]", array.toString());
+    }
 
 }
